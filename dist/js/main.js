180a8f0394614c64796353cbe9e0513e7d953cfd
oronc <oronculzac@gmail.com>
2018-09-12 20:32:26 -0400

    Initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,124 @@
+// Init Speech Synth API
+const synth = window.speechSynthesis;
+
+// Grabbing the necessary DOM Elements
+const textForm = document.querySelector('form');
+const textInput = document.querySelector('#text-input');
+const voiceSelect = document.querySelector('#voice-select');
+const rate = document.querySelector('#rate');
+const rateValue = document.querySelector('#rate-value');
+const pitch = document.querySelector('#pitch');
+const pitchValue = document.querySelector('#pitch-value');
+const body = document.querySelector('body');
+
+// Init voices array
+let voices = []; //Init empty voices array
+
+/* 
+    How to actually get the voices
+const getVoices = () => { // getVoices Function ES6
+    //take voices array and set to synth
+    voices = synth.getVoices(); //Loaded async
+    console.log(voices);
+
+};
+
+getVoices();
+if(synth.onvoiceschanged !== undefined){
+    synth.onvoiceschanged = getVoices;
+} */
+
+const getVoices = () => {
+  // getVoices Function ES6
+  //take voices array and set to synth
+  voices = synth.getVoices(); //Loaded
+
+  // Loop through voices and create option for each voice
+  voices.forEach(voice => {
+    // Create option element
+    const option = document.createElement('option');
+    // Fill option with voice and language
+    option.textContent = voice.name + '(' + voice.lang + ')';
+
+    // Set needed option attributes
+    option.setAttribute('data-lang', voice.lang);
+    option.setAttribute('data-name', voice.name);
+
+    // Append options to the select list
+    voiceSelect.appendChild(option);
+  });
+};
+
+getVoices();
+if (synth.onvoiceschanged !== undefined) {
+  synth.onvoiceschanged = getVoices;
+}
+
+// Speak
+
+const speak = () => {
+  // Check if speaking
+  if (synth.speaking) {
+    console.error('Already speaking...');
+    return;
+  }
+
+  // Make sure input isn't empty string
+  if (textInput.value !== '') {
+    // Add backgroun animation
+    body.style.background = '#141414 url(img/wave.gif)';
+    body.style.backgroundRepeat = 'repeat-x';
+    body.style.backgroundSize = '100% 100%';
+    body.style.backgroundPosition = 'top';
+
+    // Get speak text
+    const speakText = new SpeechSynthesisUtterance(textInput.value);
+    // Speak end
+    speakText.onend = e => {
+      console.log('Done speaking ...');
+      body.style.background = '#141414';
+    };
+
+    // Speak error
+    speakText.onerror = e => {
+      console.error('Something went wrong');
+    };
+
+    // Selected voice
+    const selectedVoice = voiceSelect.selectedOptions[0].getAttribute(
+      'data-name'
+    );
+
+    // Loop through voices
+    voices.forEach(voice => {
+      if (voice.name === selectedVoice) {
+        speakText.voice = voice;
+      }
+    });
+
+    // Set pitch and rate
+    speakText.rate = rate.value;
+    speakText.pitch = pitch.value;
+
+    // Speak
+    synth.speak(speakText);
+  }
+};
+
+/* EVENT LISTENERS */
+
+// Text form submit
+
+textForm.addEventListener('submit', e => {
+  e.preventDefault(); // Prevent from saving to file
+  speak();
+  textInput.blur();
+});
+
+// Rate value change
+rate.addEventListener('change', e => (rateValue.textContent = rate.value));
+// Pitch value change
+pitch.addEventListener('change', e => (pitchValue.textContent = pitch.value));
+
+// Voice select change
+voiceSelect.addEventListener('change', e => speak());
